f49d14e1a346aacf82689bfb70c1182426edc7eb
farhanfitrahtur <farhanfitrahtur@gmail.com>
2020-10-13 08:32:37 +0700

    add number boolean

---
+++ build/number_boolean.js
@@ -0,0 +1,10 @@
+"use strict";
+//number
+var number = 1;
+number = 1;
+console.log(number);
+//boolean
+var isSuccess;
+isSuccess = false;
+isSuccess = true;
+console.log("is success " + isSuccess);
---
+++ src/number_boolean.ts
@@ -0,0 +1,10 @@
+//number
+let number  = 1;
+number = 1;
+console.log(number)
+
+//boolean
+let isSuccess:boolean;
+isSuccess = false;
+isSuccess = true;
+console.log(`is success ${isSuccess}`)
