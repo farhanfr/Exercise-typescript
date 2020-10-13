eae5f567246d1a0ef646cf22062818e3d2c44884
farhanfitrahtur <farhanfitrahtur@gmail.com>
2020-10-13 10:09:55 +0700

    excer function

---
+++ build/function.js
@@ -0,0 +1,5 @@
+"use strict";
+function getName(name) {
+    return "nama : " + name;
+}
+console.log(getName("farhan"));
---
+++ src/function.ts
@@ -0,0 +1,5 @@
+function getName(name:string):string {
+    return `nama : ${name}`;
+}
+
+console.log(getName("farhan"))
